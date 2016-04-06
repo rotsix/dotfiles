2101857723412dd140c7211b62f708e66b8f902d
VictorFranzi <victor.franzi@gmail.com>
2016-04-06 12:40:16 +0200

    new link for battery notifier

--- bin/bat.sh
+++ bin/bat.sh
@@ -2,12 +2,12 @@
 
 actual="$(cat /sys/class/power_supply/BAT0/capacity)"
 
-if [ $actual -eq "100" ] #  &&  cat /sys/class/power_supply/BAT0/status -eq "Charging" ]
+if [ "$actual" -eq "100" -a "$(cat /sys/class/power_supply/BAT0/status)" = "Charging" ]
 then
 	notify-send --urgency=critical 'Batterie pleine' 'Retirez le chargeur'
 fi
 
-if [ $actual -le "15" ] # &&  cat /sys/class/power_supply/BAT0/status -eq "Discharging" ]
+if [ "$actual" -le "15" -a "$(cat /sys/class/power_supply/BAT0/status)" = "Discharging" ]
 then
 	notify-send --urgency=critical 'Batterie faible !' 'Branchez le chargeur'
 fi
