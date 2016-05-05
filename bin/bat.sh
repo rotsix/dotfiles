324a115bf3f188ca4844e98f93c9410f649bfbb7
VictorFranzi <victor.franzi@gmail.com>
2016-05-05 13:03:43 +0200

    some changes

--- bin/bat.sh
+++ bin/bat.sh
@@ -4,11 +4,11 @@ actual="$(cat /sys/class/power_supply/BAT0/capacity)"
 
 if [ "$actual" -eq "100" -a "$(cat /sys/class/power_supply/BAT0/status)" = "Charging" ]
 then
-	notify-send --urgency=critical 'Batterie pleine' 'Retirez le chargeur'
+	notify-send --urgency=critical 'Batterie pleine' 'Retirez le chargeur' -t 5000
 fi
 
 if [ "$actual" -le "15" -a "$(cat /sys/class/power_supply/BAT0/status)" = "Discharging" ]
 then
-	notify-send --urgency=critical 'Batterie faible !' 'Branchez le chargeur'
+	notify-send --urgency=critical 'Batterie faible !' 'Branchez le chargeur' -t 5000
 fi
 
