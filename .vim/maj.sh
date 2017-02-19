56fd7468e2733b70df839a51c8d65991a829b990
rotsix <victor.franzi@gmail.com>
2017-02-20 00:30:07 +0100

    fk it, made it in french

--- .vim/maj.sh
+++ .vim/maj.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-echo "MaJ plugins :"
+echo "Plugins are updating:"
 cd bundle
 for dir in $(ls); do
-	echo -e "\033[34m$dir\033[0m"
+	echo -e "Updating \033[34m$dir\033[0m:"
 	cd $dir && git pull && cd ..
 	echo ""
 done
 cd ..
 
-echo -e "\033[31mMaJ pathogen :\033[0m"
+echo -e "Updating \033[31mpathogen\033[0m:"
 cd pathogen && git pull && cd ..
 
-echo -e "\n\nMaJ terminée !"
+echo -e "\n\nAll your plugins are now up-to-date."
