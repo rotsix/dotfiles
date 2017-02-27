f27e1094466a63e475bfa40b4761708b9765d995
rotsix <victor.franzi@gmail.com>
2017-02-27 13:06:34 +0100

    packages

--- .config/polybar/pkg.sh
+++ .config/polybar/pkg.sh
@@ -5,5 +5,7 @@ aur=$(cower -u | wc -l 2> /dev/null)
 check=$((pac + aur))
 if [[ "$check" != "0" ]]
 then
-    echo "$pac %{F#5b5b5b}%{F-} $aur"
+    echo -n "$pac %{F#5b5b5b}%{F-} $aur"
+else
+    echo -n ""
 fi
