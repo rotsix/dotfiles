c0ae23ca023c14ecb47dcc4ad73beabef54fe8a2
rotsix <victor.franzi@gmail.com>
2017-08-05 02:13:44 +0200

    even faster, just ./install.sh (look at your hostname)

--- install.sh
+++ install.sh
@@ -3,29 +3,19 @@
 # rotsix - (c) wtfpl 2017
 # trololo install script
 
-usage () {
-  "./install.sh <ssh|http> <qualia qualities>"
-}
-
-
 echo "Cloning dotfiles in ~/dotfiles"
-case "$1" in
-  ssh)
+case "$(hostname)" in
+  "fry"|"lili"|"isaac")
   	git clone "git@github.com:rotsix/dotfiles.git" "$HOME/dotfiles" &> /dev/null
   	;;
-  http)
-	git clone "https://github.com/rotsix/dotfiles.git" "$HOME/dotfiles" &> /dev/null
-  	;;
   *)
-  	echo "Failed"
-  	usage
-  	exit 127
+	git clone "https://github.com/rotsix/dotfiles.git" "$HOME/dotfiles" &> /dev/null
   	;;
 esac
 shift
 
 cd "$HOME/dotfiles" && git config filter.qualia.clean qualia
-cd "$HOME/dotfiles" && git config filter.qualia.smudge "qualia $*"
+cd "$HOME/dotfiles" && git config filter.qualia.smudge "qualia $(hostname)"
 echo "Done"
 
 echo "Managing simlinks"
