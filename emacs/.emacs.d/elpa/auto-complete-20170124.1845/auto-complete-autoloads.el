d1db4a9e6fb2ec29eea4c95c7c29d1fe02be0b29
rotsix <rotsix@openmailbox.org>
2017-10-06 11:35:22 +0200

    emacs: archives and co

--- emacs/.emacs.d/elpa/auto-complete-20170124.1845/auto-complete-autoloads.el
+++ emacs/.emacs.d/elpa/auto-complete-20170124.1845/auto-complete-autoloads.el
@@ -3,9 +3,9 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "auto-complete" "auto-complete.el" (22987 38747
-;;;;;;  61082 787000))
-;;; Generated autoloads from auto-complete.el
+;;;### (autoloads nil "auto-complete" "../../../../../.emacs.d/elpa/auto-complete-20170124.1845/auto-complete.el"
+;;;;;;  "0f564b17b1ef2e700470197f0218dc22")
+;;; Generated autoloads from ../../../../../.emacs.d/elpa/auto-complete-20170124.1845/auto-complete.el
 
 (autoload 'auto-complete "auto-complete" "\
 Start auto-completion at current point.
@@ -41,9 +41,9 @@ See `auto-complete-mode' for more information on Auto-Complete mode.
 
 ;;;***
 
-;;;### (autoloads nil "auto-complete-config" "auto-complete-config.el"
-;;;;;;  (22987 38748 397749 475000))
-;;; Generated autoloads from auto-complete-config.el
+;;;### (autoloads nil "auto-complete-config" "../../../../../.emacs.d/elpa/auto-complete-20170124.1845/auto-complete-config.el"
+;;;;;;  "ff41e174795402bf5f86dfbc7e9060d2")
+;;; Generated autoloads from ../../../../../.emacs.d/elpa/auto-complete-20170124.1845/auto-complete-config.el
 
 (autoload 'ac-config-default "auto-complete-config" "\
 
@@ -52,8 +52,11 @@ See `auto-complete-mode' for more information on Auto-Complete mode.
 
 ;;;***
 
-;;;### (autoloads nil nil ("auto-complete-pkg.el") (22987 38746 967749
-;;;;;;  454000))
+;;;### (autoloads nil nil ("../../../../../.emacs.d/elpa/auto-complete-20170124.1845/auto-complete-autoloads.el"
+;;;;;;  "../../../../../.emacs.d/elpa/auto-complete-20170124.1845/auto-complete-config.el"
+;;;;;;  "../../../../../.emacs.d/elpa/auto-complete-20170124.1845/auto-complete-pkg.el"
+;;;;;;  "../../../../../.emacs.d/elpa/auto-complete-20170124.1845/auto-complete.el")
+;;;;;;  (22999 19863 451434 374000))
 
 ;;;***
 
--- emacs/.emacs.d/elpa/popup-20160709.729/popup-autoloads.el
+++ emacs/.emacs.d/elpa/popup-20160709.729/popup-autoloads.el
@@ -3,7 +3,9 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil nil ("popup.el") (22975 36534 940298 131000))
+;;;### (autoloads nil nil ("../../../../../.emacs.d/elpa/popup-20160709.729/popup-autoloads.el"
+;;;;;;  "../../../../../.emacs.d/elpa/popup-20160709.729/popup.el")
+;;;;;;  (22999 19859 958101 117000))
 
 ;;;***
 
