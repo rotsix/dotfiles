366564637ca6b9b7621012822985e112efb456cc
rotsix <rotsix@openmailbox.org>
2017-10-13 15:52:24 +0200

    emacs doom

--- emacs/.emacs.d/elpa/popup-20160709.729/popup-autoloads.el
+++
@@ -1,17 +0,0 @@
-;;; popup-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("../../../../../.emacs.d/elpa/popup-20160709.729/popup-autoloads.el"
-;;;;;;  "../../../../../.emacs.d/elpa/popup-20160709.729/popup.el")
-;;;;;;  (22999 19859 958101 117000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; popup-autoloads.el ends here
