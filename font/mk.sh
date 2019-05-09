febaade8f4332256b8fb2f63bd131cc22126a946
Victor Franzi <victor.franzi@gmail.com>
2019-05-09 22:07:43 +0200

    font: using ~/.local/share/font instead of ~/.fonts

--- font/mk.sh
+++ font/mk.sh
@@ -9,11 +9,11 @@ pip install fontTools &> /dev/null
 echo "customizing font"
 rm -rf $dir/Output_Fonts &> /dev/null
 mkdir $dir/Output_Fonts &> /dev/null
-mkdir ~/.fonts &> /dev/null
+mkdir ~/.local/share/fonts &> /dev/null
 shopt -s globstar
 python $dir/Scripts/inputCustomize.py $dir/Input_Fonts/**/*.ttf --dest=$dir/Output_Fonts --a --g --asterisk &> /dev/null
 echo "generating cache"
-cp $dir/Output_Fonts/**/*.ttf ~/.fonts &> /dev/null
+cp $dir/Output_Fonts/**/*.ttf ~/.local/share/fonts &> /dev/null
 fc-cache -fv &> /dev/null
 echo "done"
 deactivate &> /dev/null
