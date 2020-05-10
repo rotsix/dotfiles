fd53c19ee5203c43cd313ee0c3d8f80b2a738069
Victor Franzi <victor.franzi@gmail.com>
2020-05-10 18:01:15 +0200

    zsh: clean aliases

--- zsh/.zsh.d/alias.zsh
+++ zsh/.zsh.d/alias.zsh
@@ -17,7 +17,7 @@ alias grep='grep --color=auto'
 alias vim='nvim'
 alias hoogle='hoogle --color=true'
 alias s='ssh'
-alias p='trizen'
+alias p='yay'
 alias g='git'
 alias agh='git add -A && git commit -am "$(hostname)" && git push'
 alias mkt='mk /tmp/$$'
@@ -26,23 +26,16 @@ alias swapy='sudo swapon $HOME/usr/swapfile'
 alias cat='bat'
 alias mutt='neomutt'
 alias dc='docker-compose'
-
-# sorry i'm drunk
-alias sl='ls'
-alias xs='cd'
+alias youtube-audio='youtube-dl -ix --audio-quality 0 --audio-format mp3'
 
 # sorting files in dir by size
 alias lh='du -hsc * | sort -hr'
 alias lha='du -hsc .* | sort -hr'
 alias lhs='(lh;lha) | sort -hr'
 
-# mh
-alias youtube-audio='youtube-dl -ix --audio-quality 0 --audio-format mp3'
-
 # pacman
-alias up='p -Syu'
 alias orphans='pacman -Qdtq | sudo pacman -Rnsc --noconfirm - 2> /dev/null || echo "Nothing to delete"'
-alias mirrorlist-update='sudo reflector --country France --country Germany --latest 3 --age 12 --protocol https --sort rate --save /etc/pacman.d/mirrorlist; rm -f /etc/pacman.d/mirrorlist.pacnew'
+alias mirrorlist-update='sudo reflector --country France --country Germany --latest 3 --age 12 --protocol https --sort rate --save /etc/pacman.d/mirrorlist; sudo rm -f /etc/pacman.d/mirrorlist.pacnew'
 alias pkgfile='sudo pacman -Fy && pacman -F'
 
 # ps dump sorted by memory
@@ -53,9 +46,6 @@ alias sudo='sudo '
 
 # rlwrap
 alias ocaml='rlwrap \ocaml'
-alias eclipse-clp='rlwrap \eclipse-clp'
-alias hopix='rlwrap $HOME/cours/M1/S1/IDP/compilation-m1-2018/flap/flap -i true -s hopix -V true'
-alias retrolix='rlwrap $HOME/cours/M1/S2/Compilation/compilation-m1-2018/flap/flap -s retrolix -d true -r true -i true'
 
 # pip
 alias pip-update="pip list --outdated --format=freeze 2> /dev/null | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U"
