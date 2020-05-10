0048399be89556eeef1cdde3b11dcd56e001d507
Victor Franzi <victor.franzi@gmail.com>
2020-05-10 18:06:33 +0200

    zsh: clean variables

--- zsh/.zsh.d/variables.zsh
+++ zsh/.zsh.d/variables.zsh
@@ -1,9 +1,11 @@
 export EDITOR=nvim
 export BROWSER=qutebrowser
+
 export PATH=$HOME/src/sh/vem:$PATH
 export PATH=$HOME/bin:$PATH
 export PATH=$HOME/.local/bin:$PATH
 export PATH=$HOME/.cargo/bin:$PATH
+
 export MAKEFLAGS=' -j 8'
 
 export HISTFILE=$HOME/.zsh_history
@@ -16,9 +18,6 @@ export GOPATH=$HOME/src/go
 export GOROOT=/usr/lib/go
 export PATH=$GOROOT/bin:$PATH
 export PATH=$GOPATH/bin:$PATH
-
-export FZF_DEFAULT_COMMAND='fd --type f -L'
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
 export NPM_PACKAGES="$HOME/.npm-packages"
 export PATH=$NPM_PACKAGES/bin:$PATH
